9a681f1e7d73eab2733436020e19b854321163b0
wunglee <wunglee@qq.com>
2023-09-23 12:03:40 -0400

    init

--- test/thinker_ai_tests/llm/test_llm_api.py
+++ test/thinker_ai_tests/llm/test_llm_api.py
@@ -6,8 +6,10 @@ from thinker_ai.llm.llm_factory import get_llm
 class TestTestGPT(asynctest.TestCase):
     async def test_a_generate_stream(self):
         llm = get_llm()
-        result = await llm.a_generate_stream("你好")
-        self.assertIsNotNone(result)
+        await llm.a_generate_stream(
+            "我用session.request()的向你发送消息，你才能记住我们之前交流的上下文是吗，能记住多少条上下文？")
+        await llm.a_generate_stream(
+            "如果我不用session.request()向你发送消息，而是每次都新建一个request，你就不能获取我们之前交流的上下文，是吗？")
 
 
 if __name__ == '__main__':
