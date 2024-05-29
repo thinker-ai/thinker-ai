7f560a2c17b08b432e4c2f8c7bd16beb45fc2b5e
wunglee <wunglee@qq.com>
2024-05-29 13:40:23 -0400

    初始可运行

--- src/thinker_ai/tasks/dynamic/service.py
+++ src/thinker_ai/tasks/dynamic/service.py
@@ -24,9 +24,9 @@ def get_mount_path(title, user_id: Optional[str] = None) -> str:
 
 
 class Service:
-    def __init__(self, title, user_id: Optional[str] = None):
-        self.deploy_path = get_deploy_path(title, user_id)
-        self.mount_path = get_mount_path(title, user_id)
+    def __init__(self, name, user_id: Optional[str] = None):
+        self.deploy_path = get_deploy_path(name, user_id)
+        self.mount_path = get_mount_path(name, user_id)
         # 在asyncio.events.py中的get_event_loop方法中，要求
         # threading.current_thread() is threading.main_thread()
         # 才能执行self.set_event_loop(self.new_event_loop()，否则event_loop为空
@@ -37,7 +37,7 @@ class Service:
             if gradio_code:
                 local_vars = {}
                 exec(gradio_code, {}, local_vars)
-                gr_blocks = local_vars.get(title)
+                gr_blocks = local_vars.get(name)
                 if gr_blocks:
                     self.gr_blocks = gr_blocks
                 else:
