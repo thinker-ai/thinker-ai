c666006f5c9cc9efa1be2da89773e524e1522dce
wunglee <wunglee@qq.com>
2024-07-28 23:39:50 -0400

    初始可运行

--- test/thinker_ai_tests/status_machine/execute_order_test.py
+++ test/thinker_ai_tests/status_machine/execute_order_test.py
@@ -6,6 +6,7 @@ from thinker_ai.status_machine.state_machine import ActionFactory, BaseStateDefi
 from thinker_ai.status_machine.status_machine_definition_repository import FileBasedStateMachineDefinitionRepository
 from thinker_ai_tests.status_machine.sample_actions import InnerStartAction
 
+
 class TestStateMachineDefinition(unittest.TestCase):
     def setUp(self):
         self.base_dir = os.path.dirname(__file__)
@@ -13,36 +14,10 @@ class TestStateMachineDefinition(unittest.TestCase):
         self.definition_repo = FileBasedStateMachineDefinitionRepository(self.base_dir, self.definitions_file_name)
         ActionFactory.register_action(InnerStartAction.get_full_class_name(), InnerStartAction)
 
-    def test_get_state_execute_order(self):
-        sorted_states_defs: List[Tuple[str, BaseStateDefinition]] =  self.definition_repo.get_state_execute_order("example_sm")
-        sorted_states=[]
-        for name, _ in sorted_states_defs:
-            sorted_states.append(name)
-
-        expected_order_1 = [
-            "example_sm.start",
-            "example_sm.A",
-            "example_sm.B",
-            "example_sm.C",
-            "example_sm.D",
-            "example_sm.E"
-        ]
-        expected_order_2 = [
-            "example_sm.start",
-            "example_sm.A",
-            "example_sm.B",
-            "example_sm.D",
-            "example_sm.E",
-            "example_sm.C"
-        ]
-        try:
-            self.assertEqual(list(sorted_states), expected_order_1)
-        except Exception:
-            self.assertEqual(list(sorted_states), expected_order_2)
-
     def test_double_layer_state_machine(self):
-        sorted_states_defs: List[Tuple[str, BaseStateDefinition]] = self.definition_repo.get_state_execute_order("example_sm")
-        sorted_states=[]
+        sorted_states_defs: List[Tuple[str, BaseStateDefinition]] = self.definition_repo.get_state_execute_order(
+            "example_sm")
+        sorted_states = []
         for name, _ in sorted_states_defs:
             sorted_states.append(name)
         expected_order_1 = [
@@ -111,6 +86,7 @@ class TestStateMachineDefinition(unittest.TestCase):
                     self.assertEqual(sorted_states, expected_order_3)
                 except Exception:
                     self.assertEqual(sorted_states, expected_order_4)
+
 
 if __name__ == '__main__':
     unittest.main()
