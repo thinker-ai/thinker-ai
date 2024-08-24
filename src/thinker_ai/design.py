4131ec2ca6e1345f37c6e56e2adea3e625c80109
wunglee <wunglee@qq.com>
2024-08-24 09:56:01 -0400

    初始可运行

--- src/thinker_ai/design.py
+++ src/thinker_ai/design.py
@@ -14,15 +14,45 @@ design_dir = Jinja2Templates(directory=design_root)
 
 
 @design_router.get("/design", response_class=HTMLResponse)
-async def design(request: Request):
+async def main(request: Request):
     return design_dir.TemplateResponse("main.html", {"request": request})
 
 
 @design_router.get("/design/list", response_class=HTMLResponse)
-async def design(request: Request):
+async def design_list(request: Request):
     return design_dir.TemplateResponse("list.html", {"request": request})
 
 
-@design_router.get("/design/create", response_class=HTMLResponse)
-async def design(request: Request):
-    return design_dir.TemplateResponse("create.html", {"request": request})
+@design_router.get("/design/one", response_class=HTMLResponse)
+async def design_one(request: Request):
+    return design_dir.TemplateResponse("one.html", {"request": request})
+
+
+@design_router.get("/design/one/requirements", response_class=HTMLResponse)
+async def design_one_requirements(request: Request):
+    return design_dir.TemplateResponse("one/requirements.html", {"request": request})
+
+
+@design_router.get("/design/one/knowledge", response_class=HTMLResponse)
+async def design_one_knowledge(request: Request):
+    return design_dir.TemplateResponse("one/knowledge.html", {"request": request})
+
+
+@design_router.get("/design/one/resource", response_class=HTMLResponse)
+async def design_one_resource(request: Request):
+    return design_dir.TemplateResponse("one/resource.html", {"request": request})
+
+
+@design_router.get("/design/one/criterion", response_class=HTMLResponse)
+async def design_one_criterion(request: Request):
+    return design_dir.TemplateResponse("one/criterion.html", {"request": request})
+
+
+@design_router.get("/design/one/solution", response_class=HTMLResponse)
+async def design_one_solution(request: Request):
+    return design_dir.TemplateResponse("one/solution.html", {"request": request})
+
+
+@design_router.get("/design/one/strategy", response_class=HTMLResponse)
+async def design_one_strategy(request: Request):
+    return design_dir.TemplateResponse("one/strategy.html", {"request": request})
--- web/script/main.js
+++ web/script/main.js
@@ -1,14 +1,10 @@
-function showContent(page, element) {
+function showContent(frame_id,menu_class_name,page, element) {
     // Update the iframe's src attribute to load the corresponding page
-    document.getElementById('content-frame').src = page;
-
+    document.getElementById(frame_id).src = page;
     // Update the active state of the menu items
-    var menuItems = document.getElementsByClassName('menu-item');
+    var menuItems = document.getElementsByClassName(menu_class_name);
     for (var i = 0; i < menuItems.length; i++) {
         menuItems[i].classList.remove('active');
     }
     element.classList.add('active');
 }
-
-
-
