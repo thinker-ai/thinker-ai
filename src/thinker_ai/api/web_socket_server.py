b9a194f3b59f3cc2df498337395b23ab2ddf06a1
wunglee <wunglee@qq.com>
2024-09-01 05:45:46 -0400

    初始可运行

--- src/thinker_ai/api/web_socket_server.py
+++ src/thinker_ai/api/web_socket_server.py
@@ -6,6 +6,7 @@ from typing import Dict
 
 # 创建 API 路由器
 socket_router = APIRouter()
+tasks = []
 
 
 class WebsocketService:
@@ -23,7 +24,46 @@ class WebsocketService:
             self.socket_clients: Dict[str, WebSocket] = {}
             self.initialized = True
 
-    async def startup(self):
+    @classmethod
+    async def connect(cls, websocket: WebSocket, user_id: str):
+        instance = cls.get_instance()
+        try:
+            # 检查现有连接是否健康
+            if user_id in instance.socket_clients:
+                existing_websocket = instance.socket_clients[user_id]
+                if existing_websocket.client_state == WebSocketState.CONNECTED:
+                    print(f"Existing WebSocket for {user_id} is still connected.close it.")
+                    # 可以选择关闭新连接或覆盖旧连接
+                    await existing_websocket.close()  # 这里选择关闭旧连接
+            await websocket.accept()  # 等待建立连接，而非等待客户端消息
+            # data = await websocket.receive_text()
+            instance.socket_clients[user_id] = websocket
+            process_to_background = instance.process_to_background_message(websocket, user_id)
+            process_to_background_task = asyncio.create_task(process_to_background)
+            tasks.append(process_to_background_task)
+        except Exception as e:
+            print(f"Exception: {e}")
+            if user_id in instance.socket_clients:
+                del instance.socket_clients[user_id]
+                await websocket.close()
+
+    async def process_to_background_message(self, websocket: WebSocket, user_id: str):
+        while True:
+            try:
+                data = await websocket.receive_text()
+                message = json.loads(data)
+                if message.get("type") == "heartbeat":
+                    continue
+                print(f"Received message from {user_id}: {message}")
+            except WebSocketDisconnect:
+                print(f"WebSocket disconnected for {user_id}")
+                if user_id in self.socket_clients:
+                    del self.socket_clients[user_id]
+                    await websocket.close()
+            except Exception as e:
+                print(f"Error receiving message from {user_id}: {e}")
+
+    async def process_to_front_message(self):
         while True:
             try:
                 user_id, message = await self.message_queue.get()
@@ -40,50 +80,23 @@ class WebsocketService:
                 print(f"Error sending message: {e}")
 
     @classmethod
-    @socket_router.websocket("/ws/{user_id}")
-    async def accept_websocket(cls, websocket: WebSocket, user_id: str):
-        instance = cls.get_instance()
-        try:
-            await websocket.accept()  # 等待建立连接，而非等待客户端消息
-            instance.socket_clients[user_id] = websocket
-            receive_task = asyncio.create_task(instance.receive_messages(websocket, user_id))
-            await receive_task
-        except WebSocketDisconnect:
-            print(f"WebSocket disconnected for {user_id}")
-        except Exception as e:
-            print(f"Exception: {e}")
-        finally:
-            if user_id in instance.socket_clients:
-                del instance.socket_clients[user_id]
-            if websocket and websocket.application_state != WebSocketState.DISCONNECTED:
-                await websocket.close()
-
-    async def receive_messages(self, websocket: WebSocket, user_id: str):
-        while True:
-            try:
-                data = await websocket.receive_text()
-                message = json.loads(data)
-                if message.get("type") == "heartbeat":
-                    continue
-                print(f"Received message from {user_id}: {message}")
-            except WebSocketDisconnect:
-                print(f"WebSocket disconnected for {user_id}")
-                break
-            except Exception as e:
-                print(f"Error receiving message from {user_id}: {e}")
-                break
-            finally:
-                if user_id in self.socket_clients:
-                    del self.socket_clients[user_id]
-                if websocket and websocket.application_state != WebSocketState.DISCONNECTED:
-                    await websocket.close()
-
-    @classmethod
-    async def send_message_to_client(cls, user_id: str, message: dict):
+    async def send_to_front(cls, user_id: str, message: dict):
+        print(f"Server received message to send to front：{user_id}")
         instance = cls.get_instance()
         await instance.message_queue.put((user_id, message))
 
     @classmethod
-    async def start_background_tasks(cls):
+    async def start_to_front_task(cls):
         instance = cls.get_instance()
-        await instance.startup()
+        to_front_task = asyncio.create_task(instance.process_to_front_message())
+        tasks.append(to_front_task)
+
+
+@socket_router.websocket("/ws/{user_id}")
+async def accept_websocket(websocket: WebSocket, user_id: str):
+    await WebsocketService.connect(websocket, user_id)
+
+
+@socket_router.post("/send_to_front/{user_id}")
+async def accept_websocket(user_id: str, message: dict):
+    await WebsocketService.send_to_front(user_id, message)
